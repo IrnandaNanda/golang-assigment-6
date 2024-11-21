e3748751864d3e3457243db4d8378c423355d7c9
IrnandaNanda <irnandatri1509@gmail.com>
2024-11-21 19:53:09 +0700

    first commit

---
+++ service/task.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"a21hc3NpZ25tZW50/model"
+	repo "a21hc3NpZ25tZW50/repository"
+)
+
+type TaskService interface {
+	Store(task *model.Task) error
+	Update(task *model.Task) error
+	Delete(id int) error
+	GetByID(id int) (*model.Task, error)
+	GetList() ([]model.Task, error)
+	GetTaskCategory(id int) ([]model.TaskCategory, error)
+}
+
+type taskService struct {
+	taskRepository repo.TaskRepository
+}
+
+func NewTaskService(taskRepository repo.TaskRepository) TaskService {
+	return &taskService{taskRepository}
+}
+
+func (s *taskService) Store(task *model.Task) error {
+	return s.taskRepository.Store(task)
+}
+
+func (s *taskService) Update(task *model.Task) error {
+	return s.taskRepository.Update(task)
+}
+
+func (s *taskService) Delete(id int) error {
+	return s.taskRepository.Delete(id)
+}
+
+func (s *taskService) GetByID(id int) (*model.Task, error) {
+	return s.taskRepository.GetByID(id)
+}
+
+func (s *taskService) GetList() ([]model.Task, error) {
+	return s.taskRepository.GetList()
+}
+
+func (s *taskService) GetTaskCategory(id int) ([]model.TaskCategory, error) {
+	return s.taskRepository.GetTaskCategory(id)
+}
